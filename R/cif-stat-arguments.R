741acda0c18a9f33c6717a739034bf30b640f580
gestimation <gestimation@gmail.com>
2026-02-10 14:42:14 +0900

    Minor update

--- R/cif-stat-arguments.R
+++ R/cif-stat-arguments.R
@@ -6,8 +6,7 @@
 #' @param data A data frame containing variables in the formula.
 #' @param weights Optional name of the weight variable in `data`. Weights must be nonnegative.
 #' @param n.risk.type Character string; one of `"weighted"`, `"unweighted"`, or `"ess"`.
-#' Controls which risk set size is returned in `$n.risk` without affecting estimates
-#' or standard errors. (default `"weighted"`).
+#' Controls which risk set size is returned in `$n.risk` without affecting estimates or SEs (default `"weighted"`).
 #' @param subset.condition Optional character string giving a logical condition to subset
 #' `data` (default `NULL`).
 #' @param na.action A function specifying the action to take on missing values (default `na.omit`).
